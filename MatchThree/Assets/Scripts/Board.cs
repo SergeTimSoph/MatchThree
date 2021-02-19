4621753b51a5df808a36b2023fe2e54d11d146be
SergeTimSoph <sergetimsoph@gmail.com>
2021-02-19 14:01:39 +0300

    Add primitive Board with Tiles

---
+++ MatchThree/Assets/Scripts/Board.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tilePrefab;
+    [SerializeField]
+    private int width;
+    [SerializeField]
+    private int height;
+
+    private Tile[,] allTiles;
+
+    private void Awake()
+    {
+        allTiles = new Tile[width,height];
+    }
+
+    private void Start()
+    {
+        SetupTiles();
+    }
+
+    private void SetupTiles()
+    {
+        for (var i = 0; i < width; i++)
+        {
+            for (var j = 0; j < height; j++)
+            {
+                var tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
+                tile.name = $"Tile {i}, {j}";
+
+                allTiles[i, j] = tile.GetComponent<Tile>();
+                tile.transform.parent = transform;
+            }
+        }
+    }
+}

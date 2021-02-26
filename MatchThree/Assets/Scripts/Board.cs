346b733b8b9509422407e496a0068b75b78dfd6f
SergeTimSoph <sergetimsoph@gmail.com>
2021-02-26 07:36:27 +0300

    Add GamePiece random generation

--- MatchThree/Assets/Scripts/Board.cs
+++ MatchThree/Assets/Scripts/Board.cs
@@ -12,19 +12,25 @@ public class Board : MonoBehaviour
     private int borderSize;
 
     [SerializeField]
+    private GameObject[] gamePiecePrefabs;
+
+    [SerializeField]
     private Camera camera;
 
     private Tile[,] allTiles;
+    private GamePiece[,] allGamePieces;
 
     private void Awake()
     {
         allTiles = new Tile[width,height];
+        allGamePieces = new GamePiece[width,height];
     }
 
     private void Start()
     {
         SetupTiles();
         SetupCamera();
+        FillRandom();
     }
 
     private void SetupTiles()
@@ -38,6 +44,7 @@ public class Board : MonoBehaviour
 
                 allTiles[i, j] = tile.GetComponent<Tile>();
                 tile.transform.parent = transform;
+                allTiles[i, j].Init(i, j, this);
             }
         }
     }
@@ -50,5 +57,29 @@ public class Board : MonoBehaviour
         var verticalSize = (float) height / 2f + (float) borderSize;
         var horizontalSize = ((float) width / 2f + (float) borderSize) / aspectRatio;
         camera.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
+
+    private GameObject GetRandomGamePiece()
+    {
+        var randomIndex = Random.Range(0, gamePiecePrefabs.Length);
+        return gamePiecePrefabs[randomIndex];
+    }
+
+    private void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.SetCoord(x,y);
+    }
+
+    private void FillRandom()
+    {
+        for (var i = 0; i < width; i++)
+        {
+            for (var j = 0; j < height; j++)
+            {
+                var randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity);
+                PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+            }
+        }
     }
 }
--- MatchThree/Assets/Scripts/GamePiece.cs
+++ MatchThree/Assets/Scripts/GamePiece.cs
@@ -1,18 +1,15 @@
 ﻿using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class GamePiece : MonoBehaviour
 {
     [SerializeField]
     private int xIndex;
     [SerializeField]
     private int yIndex;
 
-    private Board board;
-
-    public void Init(int xIndex, int yIndex, Board board)
+    public void SetCoord(int xIndex, int yIndex)
     {
         this.xIndex = xIndex;
         this.yIndex = yIndex;
-        this.board = board;
     }
 }
